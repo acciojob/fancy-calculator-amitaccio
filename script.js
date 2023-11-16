a2e61231b58f65b9dad332be9559283046d759da
amitaccio <142372667+amitaccio@users.noreply.github.com>
2023-11-16 21:25:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let input1 = prompt("enter the first value") ;
+let var1 = parseInt(input1)*2;
+
+let input2 =  prompt("enter the second value");
+	let var2 = parseInt(input2);
+
+
+	let result = var1 + var2;
+
+alert("The final result is: " + result);
